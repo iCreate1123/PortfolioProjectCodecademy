0dc7953b128b4281f940a1ebc355c1a77587d28a
iCreate1123 <ivayloa1123@gmail.com>
2024-01-25 19:24:11 +0000

    start js

---
+++ script.js
@@ -0,0 +1,16 @@
+let stoicQuotes = document.getElementById("stoic-quotes")
+let driversClub = document.getElementById("drivers-club")
+let iShoot = document.getElementById("iShoot")
+
+// let stoicQuotesInfo = document.getElementById("stoic-description")
+
+let stoicContainer = document.getElementById("stoic-container")
+
+stoicContainer.style.display = "none"
+
+
+
+
+function popUp (click){
+    
+// 
