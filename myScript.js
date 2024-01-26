e7e4398ad17d14464dac6b821c049fd4f58df0e3
iCreate1123 <ivayloa1123@gmail.com>
2024-01-26 23:51:21 +0000

    working on js

---
+++ myScript.js
@@ -0,0 +1,17 @@
+let stoicQuotes = document.getElementById("stoic-quotes")
+let driversClub = document.getElementById("drivers-club")
+let iShoot = document.getElementById("iShoot")
+
+
+let stoicContainer = document.getElementById("stoic-container");
+// let projectsParagraphs = document.getElementsByClassName("projects-info");
+stoicContainer.style.display = "none"
+
+stoicQuotes.addEventListener("click", function() {
+    stoicContainer.style.display = "block";
+    stoicQuotes.style.display = "none";
+})
+
+
+
+console.log(projectsParagraphs) 
